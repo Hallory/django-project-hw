0c9b9346a979526bbc7991682d4c9282120240db
Hallory <khalpeep@gmail.com>
2026-03-22 19:59:46 +0100

    fix: update viev structure to apply two methods (POST, GET) in tasks func

--- hw_project/urls.py
+++ hw_project/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import create_task,get_all_tasks,get_task_by_id, get_statistics
+from .views import tasks,get_task_by_id, get_statistics
 
 urlpatterns = [
-    path("tasks/", create_task),
-    path("tasks/", get_all_tasks),
+    path("tasks/", tasks),
     path("task/<int:task_id>/", get_task_by_id),
     path("statistics/", get_statistics),
     ]
--- hw_project/views.py
+++ hw_project/views.py
@@ -11,21 +11,21 @@ from hw_project.models import Task
 from .serializers import TaskSerializer
 
 
-@api_view(["POST"])
-def create_task(request):
-    raw_data = request.data
-    serializer = TaskSerializer(data=raw_data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-def get_all_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+@api_view(["GET", "POST"])
+def tasks(request):
+    if request.method == "GET":
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
