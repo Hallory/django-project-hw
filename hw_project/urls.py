bdfe94acf5febd049ec47a7176cfb7176d82ae06
Hallory <khalpeep@gmail.com>
2026-03-22 19:06:00 +0100

    feat: add DRF task API with CRUD and statistics endpoint

--- hw_project/urls.py
+++ hw_project/urls.py
@@ -1,4 +1,9 @@
-from .views import index
 from django.urls import path
+from .views import create_task,get_all_tasks,get_task_by_id, get_statistics
 
-urlpatterns = [path("", index)]
+urlpatterns = [
+    path("tasks/", create_task),
+    path("tasks/", get_all_tasks),
+    path("task/<int:task_id>/", get_task_by_id),
+    path("statistics/", get_statistics),
+    ]
--- hw_project/views.py
+++ hw_project/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.utils import timezone
+from django.db.models import Q, Count
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the hw_project index.")
+
+from hw_project.models import Task
+
+from .serializers import TaskSerializer
+
+
+@api_view(["POST"])
+def create_task(request):
+    raw_data = request.data
+    serializer = TaskSerializer(data=raw_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def get_all_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_task_by_id(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = TaskSerializer(task)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_statistics(request):
+    tasks_by_status = Task.objects.values("status").annotate(count=Count('id')).values("status", "count")
+    return Response(
+        {
+            "total_tasks": Task.objects.count(),
+            "tasks_by_status": tasks_by_status,
+            "expired_tasks": Task.objects.filter(Q(deadline__lt=timezone.now()) & ~Q(status="done")).count()
+        }
+    )
